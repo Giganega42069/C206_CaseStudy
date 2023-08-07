4b6313d077794b35b521803d9b9fc94e06f66130
chickennuggetcurrysauce <22001095@myrp.edu.sg>
2023-08-07 10:01:32 +0800

    aloysius test cases for attendance

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -13,9 +13,14 @@ public class C206_CaseStudyTest {
 	private Activity activity1;
 	private Activity activity2;
 	private Activity activity3;
+	private Attendance attendance1;
+	private Attendance attendance2;
+	private Attendance attendance3;
 	
 	private ArrayList<User> userList;
 	private ArrayList<Activity> activityList;
+	private ArrayList<Attendance> attendanceList;
+	
 	
 	public C206_CaseStudyTest() {
 		super();
@@ -29,8 +34,13 @@ public class C206_CaseStudyTest {
 		activity1 = new Activity("Chess");
 		activity2 = new Activity("Badminton");
 		activity3 = new Activity("Volleyball");
+		attendance1 = new Attendance(user1, activity2, true);
+		attendance2 = new Attendance(user2, activity3, true);
+		attendance3 = new Attendance(user2, activity3, true);
+		
 		
 		userList = new ArrayList<User>();
+		attendanceList = new ArrayList<Attendance>();
 	}
 	
 	@Test
@@ -91,6 +101,64 @@ public class C206_CaseStudyTest {
 		userList.remove(user3);
 		assertEquals("check that the user arraylist size is 0", 0,userList.size());
 	}
+	
+	@Test
+	public void testAddAttendance() {
+		assertNotNull("Check if there is a valid attendance arraylist to add to", attendanceList);
+		attendanceList.add(attendance1);
+		assertEquals("check that the attendance arraylist size is 1", 1,attendanceList.size());
+		assertSame("check that the attendance is added", attendance1,attendanceList.get(0));
+		
+		assertNotNull("Check if there is a valid attendance arraylist to add to", attendanceList);
+		attendanceList.add(attendance2);
+		assertEquals("check that the attendance arraylist size is 2", 2,attendanceList.size());
+		assertSame("check that the attendance is added", attendance2,attendanceList.get(1));
+		
+		assertNotNull("Check if there is a valid attendance arraylist to add to", attendanceList);
+		attendanceList.add(attendance3);
+		assertEquals("check that the attendance arraylist size is 3", 3,attendanceList.size());
+		assertSame("check that the attendance is added", attendance3,attendanceList.get(2));
+		
+	
+	}
+	
+	
+	@Test
+	public void testViewAllAttendance() {
+		assertNotNull("Check if the arraylist still exist even though empty", attendanceList);
+		
+		assertNotNull("Check if there is a valid attendance arraylist to add to", attendanceList);
+		attendanceList.add(attendance1);
+		assertEquals("check that the attendance arraylist size is 2", 1,attendanceList.size());
+		assertSame("check that the attendance is added into the arraylist according to input", attendance1,attendanceList.get(0));
+		
+		assertNotNull("Check if there is a valid attendance arraylist to add to", attendanceList);
+		attendanceList.add(attendance2);
+		assertEquals("check that the attendance arraylist size is 2", 2,attendanceList.size());
+		assertSame("check that the attendance is added into the arraylist according to input", attendance2,attendanceList.get(1));
+		
+	}
+	
+
+	@Test
+	public void testDeleteAttendance() {
+		
+		attendanceList.add(attendance1);
+		assertNotNull("Check if there is a valid user arraylist to add to", userList);
+		attendanceList.remove(attendance1);
+		assertEquals("check that the user arraylist size is 0", 0,attendanceList.size());
+		
+		attendanceList.add(attendance2);
+		assertNotNull("Check if there is a valid user arraylist to add to", attendanceList);
+		attendanceList.remove(attendance2);
+		assertEquals("check that the user arraylist size is 0", 0,attendanceList.size());
+		
+		
+		assertNotNull("Check if there is a valid user arraylist to add to", attendanceList);
+		attendanceList.remove(attendance3);
+		assertEquals("check that the user arraylist size is 0", 0,attendanceList.size());
+	}
+	
 	
 	@After
 	public void tearDown() throws Exception {
